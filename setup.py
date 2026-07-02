c1179f782a9a3539d6be6a2abbc42974fd31f6f1
yusi <1179610268@qq.com>
2026-07-02 23:40:30 +0800

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="langchain-tool-gate",
-    version="0.1.0",
+    version="1.0.0",
     description="Tool governance and auditing framework for LangChain",
     long_description=long_description,
     long_description_content_type="text/markdown",
